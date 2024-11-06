cf396be4ef68e85781f43fd128632c3f15295c0e
anuri-el <sofia.poteychuk@gmail.com>
2024-11-06 19:33:15 +0200

    add a book page

--- books/urls.py
+++ books/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'books'
+
 urlpatterns = [
-    path('', views.books_list),
+    path('', views.books_list, name="catalog"),
+    path('<slug:slug>', views.book_page, name="page"),
 ]
--- books/views.py
+++ books/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from .models import Book
+from django.http import HttpResponse
 
 # Create your views here.
 def books_list(request):
     books = Book.objects.all()
     return render(request, 'books/books_list.html', {'books':books})
+
+
+def book_page(request, slug):
+    book = Book.objects.get(slug=slug)
+    return render(request, 'books/book_page.html', {'book' : book})
