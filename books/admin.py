70a974c82dbbac9387c726b98bce1fa959c87c28
Anchoys19 <anna873637@gmail.com>
2024-11-11 22:35:53 +0200

    books search

--- books/admin.py
+++ books/admin.py
@@ -3,20 +3,24 @@ from .models import Book, Author, Publisher, Genre
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
+    search_fields = ['title', 'author__first_name', 'author__last_name'] 
     list_display = ('title', 'author', 'genre', 'price', 'publication_date') 
     list_filter = ('author', 'genre', 'publication_date')  
 
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
+    search_fields = ['first_name', 'last_name'] 
     list_display = ('first_name', 'last_name', 'nationality', 'birth_date')  
     list_filter = ('nationality', 'birth_date')  
 
 @admin.register(Publisher)
 class PublisherAdmin(admin.ModelAdmin):
+    search_fields = ['title', 'address']
     list_display = ('title', 'address', 'year_founded')
     list_filter = ('year_founded',)  
 
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
+    search_fields = ['title']
     list_display = ('title', 'target_audience', 'literature_type') 
     list_filter = ('target_audience', 'literature_type')  
