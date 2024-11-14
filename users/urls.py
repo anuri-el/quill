81517701fad51266d3c274ed87c037ce454fd96d
anuri-el <sofia.poteychuk@gmail.com>
2024-11-14 16:09:05 +0200

    upgrade registration

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from . import views
+from .views import register_view, login_view, logout_view
 
 app_name = 'users'
 
 urlpatterns = [
-    path('register/', views.register_view, name="register"),
-    path('login/', views.login_view, name="login"),
-    path('logout/', views.logout_view, name="logout"),
+    path('register/', register_view, name="register"),
+    path('login/', login_view, name="login"),
+    path('logout/', logout_view, name="logout"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,16 +1,28 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
+from .forms import CustomUserCreationForm
 
 # Create your views here.
+# def register_view(request):
+#     if request.method == "POST":
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             login(request, form.save())
+#             return redirect("books:catalog")
+#     else:
+#         form = UserCreationForm()
+#     return render(request, 'users/register.html', {"form" : form})
+
 def register_view(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            login(request, form.save())
+            user = form.save()
+            login(request, user)
             return redirect("books:catalog")
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     return render(request, 'users/register.html', {"form" : form})
 
 def login_view(request):
